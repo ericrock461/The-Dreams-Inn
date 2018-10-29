684586df4c4da886032f279fa8e0c8c1803aa641
ericrock461 <ericrock461@ed.amdsb.ca>
2018-10-29 14:53:57 -0400

    Add project files.

---
+++ The Dreams Inn/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace The_Dreams_Inn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Location = new System.Drawing.Point(373, 348);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(27, 13);
+            this.redLabel.TabIndex = 0;
+            this.redLabel.Text = "Red";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Location = new System.Drawing.Point(373, 395);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(36, 13);
+            this.greenLabel.TabIndex = 1;
+            this.greenLabel.Text = "Green";
+            this.greenLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.AutoSize = true;
+            this.yellowLabel.Location = new System.Drawing.Point(61, 348);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(38, 13);
+            this.yellowLabel.TabIndex = 2;
+            this.yellowLabel.Text = "Yellow";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(173, 309);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(28, 13);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "Text";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Location = new System.Drawing.Point(61, 395);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(28, 13);
+            this.blueLabel.TabIndex = 4;
+            this.blueLabel.Text = "Blue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 417);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.redLabel);
+            this.Name = "Form1";
+            this.Text = "The Dreams Inn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label blueLabel;
+    }
+}
+
---
+++ The Dreams Inn/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Dreams_Inn
+{
+    public partial class Form1 : Form
+    {
+        // tracks what part of the game the user is at
+        int scene = 0;
+
+        // random number generator
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //display initial message and options
+            outputLabel.Text = "Press any button to start";          
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            /// to the next appropriate scene
+
+            if (e.KeyCode == Keys.N) //yellow button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+            }
+            else if (e.KeyCode == Keys.M) //red button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 3;
+                }
+
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+            }
+            else if (e.KeyCode == Keys.Space) //green button press 
+            {
+
+            }
+
+
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                case 0:
+                    outputLabel.Text = "";
+                    yellowLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 1:
+                    outputLabel.Text = "For what purpose will you be staying at the\n" +
+                        "Dreams Inn today?";
+
+                    yellowLabel.Text = "To have fun";                        
+                    redLabel.Text = "I want an exciting experience!";
+                    blueLabel.Text = "I'm just curious...";
+                    greenLabel.Text = "I won't be.";
+                    break;
+                case 2:
+                    outputLabel.Text = "Ded";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //OOPS :(
+        }
+    }
+}
