95be9f91b1838813524cfb4b46cb3bb0a32c6fc5
ericrock461 <ericrock461@ed.amdsb.ca>
2018-10-30 14:55:11 -0400

    it's broken again...

--- The Dreams Inn/Form1.Designer.cs
+++ The Dreams Inn/Form1.Designer.cs
@@ -33,59 +33,120 @@
             this.yellowLabel = new System.Windows.Forms.Label();
             this.outputLabel = new System.Windows.Forms.Label();
             this.blueLabel = new System.Windows.Forms.Label();
+            this.outputImageLabel = new System.Windows.Forms.Label();
+            this.yButton = new System.Windows.Forms.Label();
+            this.rButton = new System.Windows.Forms.Label();
+            this.bButton = new System.Windows.Forms.Label();
+            this.gButton = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // redLabel
             // 
-            this.redLabel.AutoSize = true;
-            this.redLabel.Location = new System.Drawing.Point(373, 348);
+            this.redLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.redLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.Location = new System.Drawing.Point(385, 375);
             this.redLabel.Name = "redLabel";
-            this.redLabel.Size = new System.Drawing.Size(27, 13);
+            this.redLabel.Size = new System.Drawing.Size(284, 32);
             this.redLabel.TabIndex = 0;
             this.redLabel.Text = "Red";
+            this.redLabel.Click += new System.EventHandler(this.redLabel_Click);
             // 
             // greenLabel
             // 
-            this.greenLabel.AutoSize = true;
-            this.greenLabel.Location = new System.Drawing.Point(373, 395);
+            this.greenLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.greenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenLabel.Location = new System.Drawing.Point(385, 417);
             this.greenLabel.Name = "greenLabel";
-            this.greenLabel.Size = new System.Drawing.Size(36, 13);
+            this.greenLabel.Size = new System.Drawing.Size(284, 29);
             this.greenLabel.TabIndex = 1;
             this.greenLabel.Text = "Green";
             this.greenLabel.Click += new System.EventHandler(this.label2_Click);
             // 
             // yellowLabel
             // 
-            this.yellowLabel.AutoSize = true;
-            this.yellowLabel.Location = new System.Drawing.Point(61, 348);
+            this.yellowLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.yellowLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowLabel.Location = new System.Drawing.Point(60, 375);
             this.yellowLabel.Name = "yellowLabel";
-            this.yellowLabel.Size = new System.Drawing.Size(38, 13);
+            this.yellowLabel.Size = new System.Drawing.Size(261, 32);
             this.yellowLabel.TabIndex = 2;
             this.yellowLabel.Text = "Yellow";
             // 
             // outputLabel
             // 
-            this.outputLabel.AutoSize = true;
-            this.outputLabel.Location = new System.Drawing.Point(173, 309);
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 299);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(28, 13);
+            this.outputLabel.Size = new System.Drawing.Size(648, 66);
             this.outputLabel.TabIndex = 3;
-            this.outputLabel.Text = "Text";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // blueLabel
             // 
-            this.blueLabel.AutoSize = true;
-            this.blueLabel.Location = new System.Drawing.Point(61, 395);
+            this.blueLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.blueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.Location = new System.Drawing.Point(60, 417);
             this.blueLabel.Name = "blueLabel";
-            this.blueLabel.Size = new System.Drawing.Size(28, 13);
+            this.blueLabel.Size = new System.Drawing.Size(261, 29);
             this.blueLabel.TabIndex = 4;
             this.blueLabel.Text = "Blue";
+            // 
+            // outputImageLabel
+            // 
+            this.outputImageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputImageLabel.Location = new System.Drawing.Point(70, 9);
+            this.outputImageLabel.Name = "outputImageLabel";
+            this.outputImageLabel.Size = new System.Drawing.Size(526, 276);
+            this.outputImageLabel.TabIndex = 5;
+            // 
+            // yButton
+            // 
+            this.yButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.yButton.Location = new System.Drawing.Point(12, 380);
+            this.yButton.Name = "yButton";
+            this.yButton.Size = new System.Drawing.Size(28, 23);
+            this.yButton.TabIndex = 6;
+            this.yButton.Text = "N";
+            this.yButton.Click += new System.EventHandler(this.yButton_Click);
+            // 
+            // rButton
+            // 
+            this.rButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.rButton.Location = new System.Drawing.Point(338, 380);
+            this.rButton.Name = "rButton";
+            this.rButton.Size = new System.Drawing.Size(31, 23);
+            this.rButton.TabIndex = 7;
+            this.rButton.Text = "M";
+            // 
+            // bButton
+            // 
+            this.bButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.bButton.Location = new System.Drawing.Point(12, 422);
+            this.bButton.Name = "bButton";
+            this.bButton.Size = new System.Drawing.Size(28, 23);
+            this.bButton.TabIndex = 8;
+            this.bButton.Text = "B";
+            // 
+            // gButton
+            // 
+            this.gButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.gButton.Location = new System.Drawing.Point(338, 417);
+            this.gButton.Name = "gButton";
+            this.gButton.Size = new System.Drawing.Size(41, 23);
+            this.gButton.TabIndex = 9;
+            this.gButton.Text = "Space";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 417);
+            this.ClientSize = new System.Drawing.Size(672, 455);
+            this.Controls.Add(this.gButton);
+            this.Controls.Add(this.bButton);
+            this.Controls.Add(this.rButton);
+            this.Controls.Add(this.yButton);
+            this.Controls.Add(this.outputImageLabel);
             this.Controls.Add(this.blueLabel);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.yellowLabel);
@@ -95,7 +156,6 @@
             this.Text = "The Dreams Inn";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -106,6 +166,11 @@
         private System.Windows.Forms.Label yellowLabel;
         private System.Windows.Forms.Label outputLabel;
         private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label outputImageLabel;
+        private System.Windows.Forms.Label yButton;
+        private System.Windows.Forms.Label rButton;
+        private System.Windows.Forms.Label bButton;
+        private System.Windows.Forms.Label gButton;
     }
 }
 
--- The Dreams Inn/Form1.cs
+++ The Dreams Inn/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace The_Dreams_Inn
 {
@@ -22,7 +23,7 @@ namespace The_Dreams_Inn
             InitializeComponent();
 
             //display initial message and options
-            outputLabel.Text = "Press any button to start";          
+            outputLabel.Text = "Press any button to start";
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -36,6 +37,18 @@ namespace The_Dreams_Inn
                 {
                     scene = 1;
                 }
+                else if (scene == 1)
+                {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
+                    scene = 3;
+                }
+                else if (scene == 6)
+                {
+                    scene = 4;
+                }
             }
             else if (e.KeyCode == Keys.M) //red button press
             {
@@ -45,9 +58,20 @@ namespace The_Dreams_Inn
                 }
                 else if (scene == 1)
                 {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
                     scene = 3;
                 }
-
+                else if (scene == 4)
+                {
+                    scene = 5;
+                }
+                else if (scene == 6)
+                {
+                    scene = 4;
+                }
             }
             else if (e.KeyCode == Keys.B)  //blue button press
             {
@@ -57,14 +81,32 @@ namespace The_Dreams_Inn
                 }
                 else if (scene == 1)
                 {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
+                    scene = 3;
+                }
+                else if (scene == 4)
+                {
+                    scene = 1;
+                }
+                else if (scene == 6)
+                {
                     scene = 4;
                 }
             }
             else if (e.KeyCode == Keys.Space) //green button press 
             {
-
+                if (scene == 0)
+                {
+                    scene = 1; //GREEN STORY IS DONE
+                }
+                else if (scene == 1)
+                {
+                    scene = 5;
+                }             
             }
-
 
             /// Display text and game options to screen based on the current scene
             switch (scene)
@@ -77,8 +119,7 @@ namespace The_Dreams_Inn
                     greenLabel.Text = "";
                     break;
                 case 1:
-                    outputLabel.Text = "For what purpose will you be staying at the\n" +
-                        "Dreams Inn today?";
+                    outputLabel.Text = "For what purpose will you be staying at the Dreams Inn today?";
 
                     yellowLabel.Text = "To have fun";                        
                     redLabel.Text = "I want an exciting experience!";
@@ -86,17 +127,37 @@ namespace The_Dreams_Inn
                     greenLabel.Text = "I won't be.";
                     break;
                 case 2:
-                    outputLabel.Text = "Ded";
-                    redLabel.Text = "";
-                    blueLabel.Text = "";
+                    outputLabel.Text = "You go to to front desk and check in\n" +
+                        "(When there are no options, press any button to advance)";                    
                     break;
                 case 3:
+                    outputLabel.Text = "Alright, you're checked in. Your room is to the left, up two floors. \n" +
+                        "Have a nice stay!";
+                    yellowLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
                     break;
                 case 4:
+                    outputLabel.Text = "Restart?";
+                    redLabel.Text = "No";
+                    blueLabel.Text = "Yes";                  
                     break;
                 case 5:
+                    outputLabel.Text = "I see";
+                    yellowLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    Thread.Sleep(2000);
+                    //this.Close;
                     break;
                 case 6:
+                    outputLabel.Text = "And so your stay at the Dreams Inn was completely normal.";
+                    yellowLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
                     break;
                 case 7:
                     break;
@@ -108,6 +169,16 @@ namespace The_Dreams_Inn
         private void label2_Click(object sender, EventArgs e)
         {
             //OOPS :(
+        }
+
+        private void redLabel_Click(object sender, EventArgs e)
+        {
+            //NOOOO
+        }
+
+        private void yButton_Click(object sender, EventArgs e)
+        {
+            //NOOooooooooOoOoOO
         }
     }
 }
