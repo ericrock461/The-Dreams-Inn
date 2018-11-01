5a097ce81ad7570778d4c4cd9eee6e5ab9f3f8ea
ericrock461 <ericrock461@ed.amdsb.ca>
2018-11-01 14:55:25 -0400

    Lots of scenes added

--- The Dreams Inn/Form1.Designer.cs
+++ The Dreams Inn/Form1.Designer.cs
@@ -44,41 +44,38 @@
             // 
             this.redLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.redLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.redLabel.Location = new System.Drawing.Point(385, 375);
+            this.redLabel.Location = new System.Drawing.Point(400, 235);
             this.redLabel.Name = "redLabel";
-            this.redLabel.Size = new System.Drawing.Size(284, 32);
+            this.redLabel.Size = new System.Drawing.Size(192, 41);
             this.redLabel.TabIndex = 0;
-            this.redLabel.Text = "Red";
             this.redLabel.Click += new System.EventHandler(this.redLabel_Click);
             // 
             // greenLabel
             // 
             this.greenLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.greenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.greenLabel.Location = new System.Drawing.Point(385, 417);
+            this.greenLabel.Location = new System.Drawing.Point(224, 287);
             this.greenLabel.Name = "greenLabel";
-            this.greenLabel.Size = new System.Drawing.Size(284, 29);
+            this.greenLabel.Size = new System.Drawing.Size(223, 41);
             this.greenLabel.TabIndex = 1;
-            this.greenLabel.Text = "Green";
             this.greenLabel.Click += new System.EventHandler(this.label2_Click);
             // 
             // yellowLabel
             // 
             this.yellowLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.yellowLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yellowLabel.Location = new System.Drawing.Point(60, 375);
+            this.yellowLabel.Location = new System.Drawing.Point(224, 184);
             this.yellowLabel.Name = "yellowLabel";
-            this.yellowLabel.Size = new System.Drawing.Size(261, 32);
+            this.yellowLabel.Size = new System.Drawing.Size(223, 41);
             this.yellowLabel.TabIndex = 2;
-            this.yellowLabel.Text = "Yellow";
             // 
             // outputLabel
             // 
             this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel.Location = new System.Drawing.Point(12, 299);
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(57, 354);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(648, 66);
+            this.outputLabel.Size = new System.Drawing.Size(559, 114);
             this.outputLabel.TabIndex = 3;
             this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -86,26 +83,27 @@
             // 
             this.blueLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.blueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.blueLabel.Location = new System.Drawing.Point(60, 417);
+            this.blueLabel.Location = new System.Drawing.Point(88, 235);
             this.blueLabel.Name = "blueLabel";
-            this.blueLabel.Size = new System.Drawing.Size(261, 29);
+            this.blueLabel.Size = new System.Drawing.Size(180, 41);
             this.blueLabel.TabIndex = 4;
-            this.blueLabel.Text = "Blue";
             // 
             // outputImageLabel
             // 
             this.outputImageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.outputImageLabel.Location = new System.Drawing.Point(70, 9);
+            this.outputImageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputImageLabel.Location = new System.Drawing.Point(57, 9);
             this.outputImageLabel.Name = "outputImageLabel";
-            this.outputImageLabel.Size = new System.Drawing.Size(526, 276);
+            this.outputImageLabel.Size = new System.Drawing.Size(559, 329);
             this.outputImageLabel.TabIndex = 5;
+            this.outputImageLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // yButton
             // 
             this.yButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.yButton.Location = new System.Drawing.Point(12, 380);
+            this.yButton.Location = new System.Drawing.Point(10, 182);
             this.yButton.Name = "yButton";
-            this.yButton.Size = new System.Drawing.Size(28, 23);
+            this.yButton.Size = new System.Drawing.Size(42, 41);
             this.yButton.TabIndex = 6;
             this.yButton.Text = "N";
             this.yButton.Click += new System.EventHandler(this.yButton_Click);
@@ -113,27 +111,27 @@
             // rButton
             // 
             this.rButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.rButton.Location = new System.Drawing.Point(338, 380);
+            this.rButton.Location = new System.Drawing.Point(619, 240);
             this.rButton.Name = "rButton";
-            this.rButton.Size = new System.Drawing.Size(31, 23);
+            this.rButton.Size = new System.Drawing.Size(41, 41);
             this.rButton.TabIndex = 7;
             this.rButton.Text = "M";
             // 
             // bButton
             // 
             this.bButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.bButton.Location = new System.Drawing.Point(12, 422);
+            this.bButton.Location = new System.Drawing.Point(12, 235);
             this.bButton.Name = "bButton";
-            this.bButton.Size = new System.Drawing.Size(28, 23);
+            this.bButton.Size = new System.Drawing.Size(42, 41);
             this.bButton.TabIndex = 8;
             this.bButton.Text = "B";
             // 
             // gButton
             // 
             this.gButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.gButton.Location = new System.Drawing.Point(338, 417);
+            this.gButton.Location = new System.Drawing.Point(619, 297);
             this.gButton.Name = "gButton";
-            this.gButton.Size = new System.Drawing.Size(41, 23);
+            this.gButton.Size = new System.Drawing.Size(41, 41);
             this.gButton.TabIndex = 9;
             this.gButton.Text = "Space";
             // 
@@ -141,17 +139,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 455);
+            this.ClientSize = new System.Drawing.Size(672, 477);
             this.Controls.Add(this.gButton);
-            this.Controls.Add(this.bButton);
             this.Controls.Add(this.rButton);
-            this.Controls.Add(this.yButton);
-            this.Controls.Add(this.outputImageLabel);
-            this.Controls.Add(this.blueLabel);
-            this.Controls.Add(this.outputLabel);
-            this.Controls.Add(this.yellowLabel);
             this.Controls.Add(this.greenLabel);
             this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.bButton);
+            this.Controls.Add(this.yButton);
+            this.Controls.Add(this.outputImageLabel);
+            this.Controls.Add(this.outputLabel);
             this.Name = "Form1";
             this.Text = "The Dreams Inn";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
--- The Dreams Inn/Form1.cs
+++ The Dreams Inn/Form1.cs
@@ -23,96 +23,148 @@ namespace The_Dreams_Inn
             InitializeComponent();
 
             //display initial message and options
-            outputLabel.Text = "Press any button to start";
+            outputLabel.Text = "Press any button to start \n\n" +
+                "(When there are no options, press Green to advance)";
+            outputImageLabel.Text = "My stay at the Dreams Inn";
+
+            yellowLabel.Visible = false;
+            redLabel.Visible = false;
+            blueLabel.Visible = false;
+            greenLabel.Visible = false;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             /// check to see what button has been pressed and advance
             /// to the next appropriate scene
+            
+            /* Remember,
+             * Yellow = Fun
+             * Red = Excite
+             * Blue = Curious
+             */
 
             if (e.KeyCode == Keys.N) //yellow button press
             {
-                if (scene == 0)
-                {
-                    scene = 1;
-                }
-                else if (scene == 1)
-                {
-                    scene = 2;
-                }
-                else if (scene == 2)
+                if (scene == 1)
                 {
                     scene = 3;
-                }
-                else if (scene == 6)
-                {
-                    scene = 4;
-                }
+                }           
             }
             else if (e.KeyCode == Keys.M) //red button press
             {
-                if (scene == 0)
+                if (scene == 1)
                 {
-                    scene = 1;
-                }
-                else if (scene == 1)
-                {
-                    scene = 2;
-                }
-                else if (scene == 2)
-                {
-                    scene = 3;
+                    scene = 7;
                 }
                 else if (scene == 4)
                 {
                     scene = 5;
                 }
-                else if (scene == 6)
+                else if (scene == 5)
                 {
-                    scene = 4;
+                    scene = 0;
+                }
+                else if (scene == 11)
+                {
+                    scene = 8;
+                }
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 14)
+                {
+                    scene = 15;
                 }
             }
             else if (e.KeyCode == Keys.B)  //blue button press
             {
-                if (scene == 0)
-                {
-                    scene = 1;
-                }
-                else if (scene == 1)
+                if (scene == 1)
                 {
                     scene = 2;
                 }
-                else if (scene == 2)
-                {
-                    scene = 3;
-                }
                 else if (scene == 4)
                 {
-                    scene = 1;
+                    scene = 0;
                 }
-                else if (scene == 6)
+                else if (scene == 11)
                 {
-                    scene = 4;
+                    scene = 9;
+                }
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 14)
+                {
+                    scene = 17;
                 }
             }
             else if (e.KeyCode == Keys.Space) //green button press 
             {
                 if (scene == 0)
                 {
-                    scene = 1; //GREEN STORY IS DONE
+                    scene = 1;
                 }
                 else if (scene == 1)
                 {
                     scene = 5;
-                }             
+                }
+                else if (scene == 2)
+                {
+                    scene = 6;
+                }
+                else if (scene == 3)
+                {
+                    scene = 4;
+                }
+                else if (scene == 6)
+                {
+                    scene = 4;
+                }
+                else if (scene == 7)
+                {
+                    scene = 11;
+                }
+                else if (scene == 8)
+                {
+                    scene = 9;
+                }
+                else if (scene == 9)
+                {
+                    scene = 10;
+                }
+                else if (scene == 12)
+                {
+                    scene = 13;
+                }
+                else if (scene == 13)
+                {
+                    scene = 14;
+                }
+                else if (scene == 15)
+                {
+                    scene = 16;
+                }
+                else if (scene == 16)
+                {
+                    scene = 4;
+                }
             }
 
             /// Display text and game options to screen based on the current scene
             switch (scene)
             {
                 case 0:
-                    outputLabel.Text = "";
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;
+
+                    outputLabel.Text = "Press any Button to Start\n\n" +
+                        "(When there are no options, press Green to advance)";
+                    outputImageLabel.Text = "My stay at the Dreams Inn";
                     yellowLabel.Text = "";
                     redLabel.Text = "";
                     blueLabel.Text = "";
@@ -120,6 +172,12 @@ namespace The_Dreams_Inn
                     break;
                 case 1:
                     outputLabel.Text = "For what purpose will you be staying at the Dreams Inn today?";
+                    outputImageLabel.Text = "";
+
+                    yellowLabel.Visible = true;
+                    redLabel.Visible = true;
+                    blueLabel.Visible = true;
+                    greenLabel.Visible = true;
 
                     yellowLabel.Text = "To have fun";                        
                     redLabel.Text = "I want an exciting experience!";
@@ -127,39 +185,182 @@ namespace The_Dreams_Inn
                     greenLabel.Text = "I won't be.";
                     break;
                 case 2:
-                    outputLabel.Text = "You go to to front desk and check in\n" +
-                        "(When there are no options, press any button to advance)";                    
+                    outputLabel.Text = "*You go to to front desk and check in*\n\n" +
+                        "'Alright, you're checked in. Your room is to the left, up two floors. \n" +
+                        "Have a nice stay!'";
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;
                     break;
                 case 3:
-                    outputLabel.Text = "Alright, you're checked in. Your room is to the left, up two floors. \n" +
-                        "Have a nice stay!";
-                    yellowLabel.Text = "";
-                    redLabel.Text = "";
-                    blueLabel.Text = "";
-                    greenLabel.Text = "";
+                    outputLabel.Text = "*You go to to front desk and check in*\n\n" +
+                        "'Alright, you're set! By the way, there's a list of activities we're doing tonight\n" +
+                        "on the board over there. Have a fun stay!'";
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;
                     break;
                 case 4:
                     outputLabel.Text = "Restart?";
+
+                    redLabel.Visible = true;
+                    blueLabel.Visible = true;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+
                     redLabel.Text = "No";
                     blueLabel.Text = "Yes";                  
                     break;
                 case 5:
                     outputLabel.Text = "I see";
-                    yellowLabel.Text = "";
-                    redLabel.Text = "";
-                    blueLabel.Text = "";
-                    greenLabel.Text = "";
+                    yellowLabel.Visible = false;
+                    redLabel.Text = "Title Screen";
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    Refresh();
                     Thread.Sleep(2000);
-                    //this.Close;
+                    //this.Close();
                     break;
                 case 6:
                     outputLabel.Text = "And so your stay at the Dreams Inn was completely normal.";
-                    yellowLabel.Text = "";
-                    redLabel.Text = "";
-                    blueLabel.Text = "";
-                    greenLabel.Text = "";
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;
                     break;
                 case 7:
+                    outputLabel.Text = " *You go to to front desk and check in*\n\n" +
+                        "'Aaand done! Oh, by the way, please don't look at the News. Thanks'";
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;                  
+                    break;
+                case 8:
+                    outputLabel.Text = "*You rethought for a moment, and decided you should\n" +
+                        "look at the News, just in case...*";
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    break;
+                case 9:                   
+                    outputImageLabel.Text = "Crazy gang thing!";
+                    outputLabel.Text = "";
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    break;
+                case 10:
+                    outputLabel.Text = "Uh oh, that sounds pretty bad, what should I do?";
+                    outputImageLabel.Text = "";                 
+                    yellowLabel.Visible = false;
+                    redLabel.Visible = true;
+                    blueLabel.Visible = true;
+                    greenLabel.Visible = false;
+
+                    redLabel.Text = "Leave";                  
+                    blueLabel.Text = "Go to room";
+                    break;
+                case 11:
+                    outputLabel.Text = "";
+                    outputImageLabel.Text = "";
+
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    redLabel.Visible = true;
+                    blueLabel.Visible = true;
+                    
+                    //sound to indicate there's a choice?
+                    redLabel.Text = "Don't look at the News";
+                    blueLabel.Text = "Look at the News";
+                    break;
+                case 12:
+                    outputLabel.Text = "*You hear a voice from behind you*\n" +
+                        "'Where do ya fink you're goin'? Cuz ya comin' wif us!!'";
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    break;
+                case 13:
+                    outputLabel.Text = "";
+                    outputImageLabel.BackColor = Color.Black;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    Refresh();
+                    Thread.Sleep(3100);
+                    
+                    greenLabel.Visible = true;
+                    greenLabel.Text = "Continue";
+                    break;
+                case 14:
+                    outputImageLabel.BackColor = Color.White;
+                    //outputImageLabelLabel.Image = 
+                    outputLabel.Text = "'Ok, so what we're gonna do, right? Is you play this here \n" +
+                        "slot machine, okay? If you win, I'll let you go. \n" +
+                         "Got it? Great.'";             
+                    outputImageLabel.Text = "";
+                    redLabel.Visible = true;
+                    blueLabel.Visible = true;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+
+                    redLabel.Text = "Hecc No!";
+                    blueLabel.Text = "...Ok...";
+                    break;
+                case 15:
+                    outputLabel.Text = "'Alright, fair enuff then.'";
+                    outputImageLabel.Text = "";
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    break;
+                case 16:
+                    outputLabel.Text = "";
+                    outputImageLabel.Text = "Game Over";
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    break;
+                case 17:
+                    outputLabel.Text = "";
+                    outputImageLabel.Text = "";
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    break;
+                case 18:
+                    outputLabel.Text = "";
+                    outputImageLabel.Text = "Game Over";
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    break;
+                case 19:
+                    outputLabel.Text = "";
+                    outputImageLabel.Text = "Game Over";
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
+                    break;
+                case 20:
+                    outputLabel.Text = "";
+                    outputImageLabel.Text = "Game Over";
+                    redLabel.Visible = false;
+                    blueLabel.Visible = false;
+                    yellowLabel.Visible = false;
+                    greenLabel.Visible = false;
                     break;
                 default:
                     break;
@@ -182,3 +383,54 @@ namespace The_Dreams_Inn
         }
     }
 }
+/*private void spinButton_Click(object sender, EventArgs e)
+{
+    reel1Value = randGen.Next(1, 4);
+    reel2Value = randGen.Next(1, 4);
+    reel3Value = randGen.Next(1, 4);
+
+    switch (reel1Value)
+    {
+        case 1:
+            reel1.Image = Properties.Resources._7_100x125;
+            break;
+        case 2:
+            reel1.Image = Properties.Resources.cherry_100x125;
+            break;
+        default:
+            reel1.Image = Properties.Resources.diamond_100x125;
+            break;
+    }
+    switch (reel2Value)
+    {
+        case 1:
+            reel2.Image = Properties.Resources._7_100x125;
+            break;
+        case 2:
+            reel2.Image = Properties.Resources.cherry_100x125;
+            break;
+        default:
+            reel2.Image = Properties.Resources.diamond_100x125;
+            break;
+    }
+    switch (reel3Value)
+    {
+        case 1:
+            reel3.Image = Properties.Resources._7_100x125;
+            break;
+        case 2:
+            reel3.Image = Properties.Resources.cherry_100x125;
+            break;
+        default:
+            reel3.Image = Properties.Resources.diamond_100x125;
+            break;
+    }
+
+    if (reel1Value == reel2Value && reel2Value == reel3Value) //&& reel1 == reel3??
+    {
+        score = score + 5;
+        scoreDisplay.Text = Convert.ToString(score);
+        outputLabel.Text = "Winner!";
+    }   
+}
+*/
